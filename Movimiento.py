d588422ecc58993d3f8e2b5dee421af258c6acc3
Carolina Gonzalez <carolay-96@live.com>
2016-06-22 21:26:07 -0500

    Flechas de direccion

--- Movimiento.py
+++ Movimiento.py
@@ -1,5 +1,10 @@
 import time
 from tkinter import *
+print("Instrucciones para mover las figuras:")
+print("Triangulo:\n utilice las flechas de direccion")
+print("Rectangulo:\n arriba = w\n abajo = x\n izquierda = a\n derecha = d")
+print("Circulo:\n arriba = u\n abajo = n\n izquierda = h\n derecha = k")
+time.sleep(10)
 tk=Tk()
 ventana= Canvas(tk, width=400, height=500)
 ventana.pack()
@@ -7,30 +12,50 @@ fig=ventana.create_polygon(20,10,20,60,80,35, fill="yellow")
 rec=ventana.create_rectangle(20,90, 80, 120, fill="blue")
 cir=ventana.create_oval(20, 200, 80, 150, fill="red")
 
-def Movimiento(event):
+def MovimientoTriangulo(event):
     if event.keysym == 'Up':
         ventana.move(fig,0,-3)
-        ventana.move(rec,0,-3)
-        ventana.move(cir,0,-3)
     elif event.keysym == 'Down':
         ventana.move(fig,0,3)
-        ventana.move(rec,0,3)
-        ventana.move(cir,0,3)
     elif event.keysym == 'Left':
         ventana.move(fig,-3,0)
-        ventana.move(rec,-3,0)
-        ventana.move(cir,-3,0)
-    else:
+    elif event.keysym == 'Right':
         ventana.move(fig, 3, 0)
-        ventana.move(rec, 3, 0)
-        ventana.move(cir,3,0)
        
+def MovimientoRectangulo(event):
+    if event.keysym == 'w':
+        ventana.move(rec,0,-3)
+    elif event.keysym == 'x':
+        ventana.move(rec,0,3)
+    elif event.keysym == 'a':
+        ventana.move(rec,-3,0)
+    elif event.keysym == 'd':
+        ventana.move(rec, 3, 0)
 
+def MovimientoCirculo(event):
+    if event.keysym == 'u':
+        ventana.move(cir,0,-3)
+    elif event.keysym == 'n':
+        ventana.move(cir,0,3)
+    elif event.keysym == 'h':
+        ventana.move(cir,-3,0)
+    elif event.keysym == 'k':
+        ventana.move(cir,3,0)
 
-ventana.bind_all('<KeyPress-Up>',Movimiento)
-ventana.bind_all('<KeyPress-Down>',Movimiento)
-ventana.bind_all('<KeyPress-Left>',Movimiento)
-ventana.bind_all('<KeyPress-Right>',Movimiento)
+ventana.bind_all('<KeyPress-Up>',MovimientoTriangulo)
+ventana.bind_all('<KeyPress-Down>',MovimientoTriangulo)
+ventana.bind_all('<KeyPress-Left>',MovimientoTriangulo)
+ventana.bind_all('<KeyPress-Right>',MovimientoTriangulo)
+
+ventana.bind_all('<w>',MovimientoRectangulo)
+ventana.bind_all('<x>',MovimientoRectangulo)
+ventana.bind_all('<a>',MovimientoRectangulo)
+ventana.bind_all('<d>',MovimientoRectangulo)
+
+ventana.bind_all('<u>',MovimientoCirculo)
+ventana.bind_all('<n>',MovimientoCirculo)
+ventana.bind_all('<h>',MovimientoCirculo)
+ventana.bind_all('<k>',MovimientoCirculo)
 ventana.move(rec,0,3)
 
 tk.mainloop()
